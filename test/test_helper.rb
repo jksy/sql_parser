bc493e3fbfd6873588c54304c8d6f4379cb89dab
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2016-08-11 19:07:14 +0900

    support 1.9.3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,14 +6,23 @@ require 'oracle-sql-parser'
 require "#{File.expand_path('./', File.dirname(__FILE__))}/parse_testable.rb"
 
 module Test::Unit::Assertions
-  AssertionMessage.max_diff_target_string_size = 10000
+  AssertionMessage.max_diff_target_string_size = 10000 if RUBY_VERSION > '2.0.0'
 
   def assert_ast_equal(expect, actual, message = nil)
-    difference = AssertionMessage.delayed_diff(expect.to_s, actual.to_s)
-    full_message = build_message(message, <<EOS, expect, actual, difference)
+    difference = nil
+    full_message = nil
+    if RUBY_VERSION > '2.0.0'
+      difference = AssertionMessage.delayed_diff(expect.to_s, actual.to_s) if RUBY_VERSION > '2.0.0'
+      full_message = build_message(message, <<EOS, expect, actual, difference)
 <?> expected but was
 <?>.?
 EOS
+    else
+      full_message = build_message(message, <<EOS, expect, actual)
+<?> expected but was
+<?>.?
+EOS
+    end
 
     assert_block(full_message) do
       expect == actual
