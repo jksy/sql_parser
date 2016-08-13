171a51a59746e11e3b94874cac039a30186b574b
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2016-08-13 20:46:12 +0900

    support ruby 1.9.3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 require 'test/unit'
 require 'test/unit/assertions'
-require 'pry-byebug'
+require 'pry-byebug' if RUBY_VERSION >= '2.0.0'
 require 'colorize'
 lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
@@ -8,7 +8,7 @@ require 'oracle-sql-parser'
 require "#{File.expand_path('./', File.dirname(__FILE__))}/parse_testable.rb"
 
 module Test::Unit::Assertions
-  AssertionMessage.max_diff_target_string_size = 10000 if RUBY_VERSION > '2.0.0'
+  AssertionMessage.max_diff_target_string_size = 10000 if RUBY_VERSION >= '2.0.0'
 
   def assert_ast_equal(expect, actual, message = nil)
     difference = nil
