8d92c4bf6ff38323474f1add07d7580d948d4d77
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2022-06-11 15:29:07 +0900

    Feature/add simplecov (#27)
    * append simplecov
    * append codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,19 @@
+require 'simplecov'
+require 'simplecov-cobertura'
+SimpleCov.start do
+  if ENV['CI']
+    formatter SimpleCov::Formatter::CoberturaFormatter
+  else
+    formatter SimpleCov::Formatter::MultiFormatter.new([
+                SimpleCov::Formatter::SimpleFormatter,
+                SimpleCov::Formatter::HTMLFormatter
+              ])
+  end
+
+  add_filter "/test/"
+  add_filter "/vendor/"
+end
+
 require 'test/unit'
 require 'test/unit/assertions'
 require 'colorize'
